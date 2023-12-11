6b903fec61ead35c32a729981ef53f590fce4fbc
Rejwan Shafi Wasef <rejwanshafi.study@gmail.com>
2023-12-12 02:46:45 +0600

    Update online_movie_watching_platform(1).sql

--- SQL/online_movie_watching_platform(1).sql
+++ SQL/online_movie_watching_platform(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2023 at 08:40 PM
+-- Generation Time: Dec 11, 2023 at 09:46 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
